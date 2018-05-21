a89461cdf8438c930deb65da60da34902ac62542
Medi Assumani <mediassumani49@gmail.com>
2018-05-20 21:12:12 -0400

    Update doubly_linked_list.py

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -8,3 +8,68 @@ class Node(object):
 
     def __init__(self,data):
         self.data = data
+
+        # The doubly linked list class
+class DoublyLinkedList(object):
+
+    def __init__(self):
+        """Initialize this doubly linked list ."""
+        self.head = None
+        self.tail = None
+
+    def string_representation(self):
+        """Return a formatted string representation of this linked list."""
+
+
+    def list_of_items(self):
+         """Return a list of all items in this linked list.
+        Best and worst case running time: Theta(n) for n items in the list
+        because we always need to loop through all n nodes."""
+
+    def is_empty(self):
+        """Return True if this linked list is empty, or False."""
+
+    def length(self):
+         """Return the length of this linked list by traversing its nodes."""
+
+    def get_at_index(self,index):
+
+        """Return the item at the given index in this linked list, or
+        raise ValueError if the given index is out of range of the list size.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+
+    def insert_at_index(self,item):
+        """Insert the given item at the given index in this linked list, or
+        raise ValueError if the given index is out of range of the list size.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+
+    def append(self,item):
+        """Insert the given item at the tail of this linked list.
+        Best and worst case running time: ??? under what conditions? [TODO]"""
+
+    def prepend(self,item):
+        """Insert the given item at the head of this linked list.
+        Best and worst case running time: ??? under what conditions? [TODO]"""
+
+    def find(self,item):
+        """Return an item from this linked list satisfying the given quality.
+        Best case running time: Omega(1) if item is near the head of the list.
+        Worst case running time: O(n) if item is near the tail of the list or
+        not present and we need to loop through all n nodes in the list."""
+
+    def replace(self, old_item, new_item):
+        """Replace the given old_item in this linked list with given new_item
+        using the same node, or raise ValueError if old_item is not found.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+
+    def delete(self,item):
+        """Delete the given item from this linked list, or raise ValueError.
+        Best case running time: ??? under what conditions? [TODO]
+        Worst case running time: ??? under what conditions? [TODO]"""
+
+    def sort_list(self):
+        """ Using Quick Sort to sort the list"""
+    
