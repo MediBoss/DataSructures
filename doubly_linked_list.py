96c21213c2274874a7208fd9df38ec8dca51dea2
Medi Assumani <mediassumani49@gmail.com>
2018-05-20 21:10:43 -0400

    Update doubly_linked_list.py

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -1,1 +1,10 @@
 
+
+    # The node class
+class Node(object):
+
+    next_pointer = None # This points to the next node
+    prev_pointer = None # This points to the previous node
+
+    def __init__(self,data):
+        self.data = data
