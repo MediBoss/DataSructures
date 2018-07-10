e9a86fc66366f6f4e95be6bd01939ea14302aeb4
MediBoss <mediassumani49@gmail.com>
2018-07-09 21:18:07 -0400

    implementing DLL

--- DoublyLinkedList/doubly_linked_list.py
+++ DoublyLinkedList/doubly_linked_list.py
@@ -1,29 +1,21 @@
 
     # The node class
-class Node(object):
+class Node:
 
-    next = None
-    previous = None
-
-    def __init__(self,data):
-        self.data = data
-
-    def representation(self):
-        return "Node with data of {} ".fomat(self.data)
+def __init__(self,data):
+    self.data = Data
+    self.next = None
+    self.prev = None
 
         # The doubly linked list class
-class DoublyLinkedList(object):
+class DoublyLinkedList:
 
-    length = 0 # class attribute
-
-    def __init__(self, items = None):
+    def __init__(self):
         """Initialize this doubly linked list ."""
         self.head = None
         self.tail = None
+        self.size = 0
 
-    def string_representation(self):
-        """Return a formatted string representation of this linked list."""
-        return "Doubly linked list with {} elemnets".fromat(DoublyLinkedList.length)
 
     def list_of_items(self):
          """Return a list of all items in this linked list"""
@@ -61,23 +53,21 @@ class DoublyLinkedList(object):
         Best case running time: ??? under what conditions? [TODO]
         Worst case running time: ??? under what conditions? [TODO]"""
 
-    def append(self,item):
+    def append(self, data):
         """Insert the given item at the tail of this linked list."""
-        temporary_node = Node(item) # creates a new node with the data passed O(1)
-        temporary_node.next = None # Sets its pointer to a value of nothing
-        self.tail.next = temporary_node # next pointer of tail links to the temp.node
-        temporary_node.previous = self.tail # the previous node of temp.node now links to former tail
-        self.tail = temporary_node # now temp node is the tail
+            pass
 
-    def prepend(self,item):
+    def prepend(self, data):
         """Insert the given item at the head of this linked list.
         Best and worst case running time: ??? under what conditions? [TODO]"""
 
-    def find(self,item):
+            pass
+
+    def find(self, data):
         """Return an item from this linked list satisfying the given quality"""
 
 
-    def replace(self, old_item, new_item):
+    def replace(self, old_data, new_data):
         """Replace the given old_item in this linked list with given new_item
         using the same node, or raise ValueError if old_item is not found.
         Best case running time: ??? under what conditions? [TODO]
