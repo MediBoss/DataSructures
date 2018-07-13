19937c4dd9af9f52fb34896e67ed651cab6dfdf3
Medi Assumani <mediassumani49@gmail.com>
2018-07-12 21:35:20 -0400

    Update doubly_linked_list.py

--- DoublyLinkedList/doubly_linked_list.py
+++ DoublyLinkedList/doubly_linked_list.py
@@ -1,22 +1,18 @@
 
-    # The node class
 class Node:
 
     def __init__(self,data):
         self.data = Data
         self.next = None
         self.prev = None
-
-        # The doubly linked list class
+        
 class DoublyLinkedList:
 
     def __init__(self):
-        """Initialize this doubly linked list ."""
         if isinstance(head, Node) and isinstance(tail, Node):
             self.head = None
 
     def list_of_items(self):
-         """Return a list of all items in this linked list"""
          assert(is_empty() == True), "The list is empty"
          try:
              list_to_be_returned = []
@@ -30,23 +26,18 @@ class DoublyLinkedList:
 
          return list_to_be_returned
 
-    # function that retruns a boolean value if the list is empty or not
     def isEmpty(self):
         return self.head is None
-
-    # This function adds teh data given at the end of the list
+    
     def append(self, data):
-        """Insert the given item at the tail of this linked list."""
+
 
         new_node = Node(data)
         new_node.next = None # since it will be the last node in the list
-
-        #If the list is empty...
         if self.head is None:
             new_node.prev = None
             self.head = new_node
             return
-        # if the list is not empty
         else:
             current_node = self.head
             while current_node.next is not None:
@@ -56,10 +47,9 @@ class DoublyLinkedList:
             new_node.prev = current_node
             return
 
-        # This function adds teh data given at the beginning of the list
         def prepand(self, data):
             new_node = Node(data)
             new_node.next = self.head
-            if !isEmpty():
+            if isEmpty() is False:
                 self.head.prev = new_node
             self.head = new_node
