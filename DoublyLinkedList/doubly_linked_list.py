ee17ab0d64a47db5e1c1cf8ee052070960540135
Medi Assumani <mediassumani49@gmail.com>
2018-07-17 21:25:18 -0400

    Update doubly_linked_list.py

--- DoublyLinkedList/doubly_linked_list.py
+++ DoublyLinkedList/doubly_linked_list.py
@@ -1,18 +1,21 @@
 
+    # The node class
 class Node:
 
     def __init__(self,data):
         self.data = Data
         self.next = None
         self.prev = None
-        
+
+        # The doubly linked list class
 class DoublyLinkedList:
 
     def __init__(self):
-        if isinstance(head, Node) and isinstance(tail, Node):
             self.head = None
+            self.tail = None
 
     def list_of_items(self):
+         """Return a list of all items in this linked list"""
          assert(is_empty() == True), "The list is empty"
          try:
              list_to_be_returned = []
@@ -26,28 +29,33 @@ class DoublyLinkedList:
 
          return list_to_be_returned
 
+    # function that retruns a boolean value if the list is empty or not
     def isEmpty(self):
-        return self.head is None
-    
+        return self.head == None
+
+    # This function adds teh data given at the end of the list
     def append(self, data):
         new_node = Node(data)
-        new_node.next = None # since it will be the last node in the list
-        if self.head is None:
-            new_node.prev = None
-            self.head = new_node
-            return
+        new_node.next = None
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+
+        # This function adds the data given at the beginning of the list
+    def prepand(self, data):
+        new_node = Node(data)
+        if isEmpty() == True:
+            self.tail = new_node
         else:
-            current_node = self.head
-            while current_node.next is not None:
-                current_node = current_node.next
-
-            current_node.next = new_node
-            new_node.prev = current_node
-            return
-
-        def prepand(self, data):
-            new_node = Node(data)
+            self.head.prev = new_node
             new_node.next = self.head
-            if isEmpty() is False:
-                self.head.prev = new_node
             self.head = new_node
+
+    #This function deletes the head Node from the list
+    def deleteHead(self):
+        if isEmpty() == True:
+            return "List is Empty"
+        else:
+            temp_node = self.head
+            temp_node.prev = None
+            self.head  = temp_node
