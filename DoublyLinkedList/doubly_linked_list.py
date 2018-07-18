c09d482d56f70573c615166ebb3f1307be8a58f4
Medi Assumani <mediassumani49@gmail.com>
2018-07-17 21:46:40 -0400

    Update doubly_linked_list.py

--- DoublyLinkedList/doubly_linked_list.py
+++ DoublyLinkedList/doubly_linked_list.py
@@ -11,40 +11,34 @@ class Node:
 class DoublyLinkedList:
 
     def __init__(self):
-            self.head = None
-            self.tail = None
-
-    def list_of_items(self):
-         """Return a list of all items in this linked list"""
-         assert(is_empty() == True), "The list is empty"
-         try:
-             list_to_be_returned = []
-             temporary_node = self.head # starting with the head node
-             while temporary_node is not None:
-                 list_to_be_returned.append(temporary_node.data)
-                 temporary_node = temp_node.next
-
-         except TypeError,IndexError:
-             print "Error Occured : Either Null value found or index out of bound"
-
-         return list_to_be_returned
+        self.head = None
+        self.tail = None
 
     # function that retruns a boolean value if the list is empty or not
     def isEmpty(self):
         return self.head == None
 
-    # This function adds teh data given at the end of the list
-    def append(self, data):
+    # This function adds the data given at the end of the list
+    def insertTail(self, data):
         new_node = Node(data)
         new_node.next = None
         self.tail.next = new_node
         new_node.prev = self.tail
         self.tail = new_node
 
+    def deleteTail(self):
+        if self.isEmpty() == True:
+            print "The List is Empty"
+        else:
+            temp_node = self.head
+            self.tail = self.tail.prev
+            self.tail.next = None
+            return temp
+
         # This function adds the data given at the beginning of the list
-    def prepand(self, data):
+    def insertHead(self, data):
         new_node = Node(data)
-        if isEmpty() == True:
+        if self.isEmpty() == True:
             self.tail = new_node
         else:
             self.head.prev = new_node
@@ -53,9 +47,28 @@ class DoublyLinkedList:
 
     #This function deletes the head Node from the list
     def deleteHead(self):
-        if isEmpty() == True:
-            return "List is Empty"
+        temp_node = self.head
+        self.head = self.head.next
+        self.head.prev = None
+        if self.isEmpty() == True:
+            self.tail = None
+            print "The List is Empty"
+        return temp
+
+    #This function deletes a Node from the list given a data
+    def delete(self, data):
+        if self.isEmpty() == True:
+            print "The List is Empty"
+            return
         else:
             temp_node = self.head
-            temp_node.prev = None
-            self.head  = temp_node
+            while(temp_node.data != data):
+                # looking for the node that has the data to be deleted
+                temp_node = temp_node.next
+            if temp_node == self.head:
+                self.deleteHead()
+            elif temp_node == self.tail:
+                self.deleteTail()
+            else:
+                temp_node.prev.next = temp_node.next
+                temp_node.next.prev = temp_node.prev
