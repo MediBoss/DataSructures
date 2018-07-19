8bf121fb5b47f1eeef48370b39d830eb5ee5a6a0
MediBoss <mediassumani49@gmail.com>
2018-07-18 21:24:32 -0400

    testing

--- DoublyLinkedList/Doubly_Linked_List_Tester.py
+++ DoublyLinkedList/Doubly_Linked_List_Tester.py
@@ -1,3 +1,12 @@
 from doubly_linked_list import DoublyLinkedList
 from doubly_linked_list import Node
 import unittest
+
+class TestNode(unittest.TestCase):
+
+    def test_init(self):
+        data =
+        node = Node(data)
+
+class TestDoublyLinkedList(unittest.TestCase):
+    
