578dd71f870fb5171cc89eff4bf12a4e6f535fea
MediBoss <mediassumani49@gmail.com>
2018-07-18 20:28:06 -0400

    adding legth() method

--- DoublyLinkedList/doubly_linked_list.py
+++ DoublyLinkedList/doubly_linked_list.py
@@ -1,6 +1,6 @@
 
     # The node class
-class Node:
+class Node(object):
 
     def __init__(self,data):
         self.data = Data
@@ -8,58 +8,100 @@ class Node:
         self.prev = None
 
         # The doubly linked list class
-class DoublyLinkedList:
+class DoublyLinkedList(object):
 
+    counter  = 0
     def __init__(self):
-        """Initialize this doubly linked list ."""
-        if isinstance(head, Node) and isinstance(tail, Node):
-            self.head = None
-
-    def list_of_items(self):
-         """Return a list of all items in this linked list"""
-         assert(is_empty() == True), "The list is empty"
-         try:
-             list_to_be_returned = []
-             temporary_node = self.head # starting with the head node
-             while temporary_node is not None:
-                 list_to_be_returned.append(temporary_node.data)
-                 temporary_node = temp_node.next
-
-         except TypeError,IndexError:
-             print "Error Occured : Either Null value found or index out of bound"
-
-         return list_to_be_returned
+        self.head = None
+        self.tail = None
 
     # function that retruns a boolean value if the list is empty or not
     def isEmpty(self):
-        return self.head is None
+        return self.head == None
 
-    # This function adds teh data given at the end of the list
-    def append(self, data):
-        """Insert the given item at the tail of this linked list."""
-
-        new_node = Node(data)
-        new_node.next = None # since it will be the last node in the list
-
-        #If the list is empty...
-        if self.head is None:
-            new_node.prev = None
-            self.head = new_node
+    def printLinkedList(self):
+        if self.isEmpty() == True:
+            print "The list is Empty"
             return
-        # if the list is not empty
         else:
-            current_node = self.head
-            while current_node.next is not None:
-                current_node = current_node.next
+            temp_node = self.head
+            while(temp_node.next != None):
+                print(temp_node.data)
 
-            current_node.next = new_node
-            new_node.prev = current_node
-            return
+    # This functions retruns a boolean on wheter or not the data is found in the list
+    def find(self, data):
+        if self.isEmpty() == True:
+            print "The List is empty"
+        else:
+            temp_node = self.head # start from the head node
+            while temp_node is not None:
+                if temp_node.data == data:
+                    return True
+                temp_node = temp_node.next
+            return False
 
-        # This function adds teh data given at the beginning of the list
-        def prepand(self, data):
-            new_node = Node(data)
+
+    def replace(self, old_item, new_item):
+        
+
+    def length(self):
+        return self.counter
+
+    # This function adds the data given at the end of the list
+    def insertTail(self, data):
+        new_node = Node(data)
+        new_node.next = None
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+        self.counter += 1
+
+    def deleteTail(self):
+        if self.isEmpty() == True:
+            print "The List is Empty"
+        else:
+            temp_node = self.head
+            self.tail = self.tail.prev
+            self.tail.next = None
+            self.counter -= 1
+            return temp
+
+        # This function adds the data given at the beginning of the list
+    def insertHead(self, data):
+        new_node = Node(data)
+        if self.isEmpty() == True:
+            self.tail = new_node
+        else:
+            self.head.prev = new_node
             new_node.next = self.head
-            if !isEmpty():
-                self.head.prev = new_node
             self.head = new_node
+        self.counter += 1
+
+    #This function deletes the head Node from the list
+    def deleteHead(self):
+        temp_node = self.head
+        self.head = self.head.next
+        self.head.prev = None
+        if self.isEmpty() == True:
+            self.tail = None
+            print "The List is Empty"
+        self.counter -= 1
+        return temp
+
+    #This function deletes a Node from the list given a data
+    def delete(self, data):
+        if self.isEmpty() == True:
+            print "The List is Empty"
+            return
+        else:
+            temp_node = self.head
+            while(temp_node.data != data):
+                # looking for the node that has the data to be deleted
+                temp_node = temp_node.next
+            if temp_node == self.head:
+                self.deleteHead()
+            elif temp_node == self.tail:
+                self.deleteTail()
+            else:
+                temp_node.prev.next = temp_node.next
+                temp_node.next.prev = temp_node.prev
