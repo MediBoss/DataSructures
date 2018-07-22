2bb9aca7617b5d578666144f57b54ed3cf491a45
MediBoss <mediassumani49@gmail.com>
2018-07-21 20:42:44 -0400

    implementing peek() method

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -12,17 +12,23 @@ class Stack(object):
             for data in iterable:
                 self.push(data)
             return
-
         self.top = None
-
+        
+    # Checks if the sack is empty or not
     def isEmpty():
         return self.head == None
 
+    # inserts  a data on top of the stack
     def push(data):
         node = Node(data)
         node.next = self.top
         self.top = node
 
+    # Pops off the node on top of the stack and returns its data
     def pop():
         self.top = self.top.next
         return top.data
+
+    # Returns the data at the top of the stack
+    def peek():
+        return self.top.data
