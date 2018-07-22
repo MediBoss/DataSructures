c482f3e36f7d3b2e94aeae208d94590387d432c0
MediBoss <mediassumani49@gmail.com>
2018-07-21 22:29:34 -0400

    Testing

---
+++ Stack/Test.py
@@ -0,0 +1,11 @@
+import unittest
+from Stack import Stack, Node
+
+class TestStack(unittest.TestCase):
+    def test_init(self):
+    def test_size(self):
+    def test_isEmpy(self):
+    def test_push(self):
+    def test_pop(self):
+    def test_peak(self):
+        
