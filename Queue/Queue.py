3878ff45b6f4c888926abb165e995c10d1c9e645
MediBoss <mediassumani49@gmail.com>
2018-07-23 20:48:23 -0400

    Queue class

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -9,6 +9,7 @@ class Node(object):
 
 class Queue(object):
 
+
     def __init__(self, iterator=None):
         if iterator is not None:
             for data in iterator:
@@ -16,3 +17,21 @@ class Queue(object):
 
         self.front = None
         self.back = None
+        self.counter = 0
+
+    # Function to return the length of the queue
+    def size(self):
+        return self.counter
+
+    # Function to to check wheater or not the list empty
+    def isEmpty(self):
+        return self.front is None
+
+    # Function to add an object at the back of the queue
+    def enqueue(self, data):
+
+    # Function to remove and return the object at the front of the queue
+    def dequeue(self, data):
+
+    # Function to retrun the object at the front of the queue
+    def peak(self):
