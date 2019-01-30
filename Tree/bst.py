12ded4a26f7f28ff35df5bdc105d436039f98cae
Medi Assumani <mediassumani49@gmail.com>
2019-01-30 05:51:12 -0800

    [Add] started learning about trees DS

---
+++ Tree/bst.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, data):
+        ''' Initialize a Node with a data, left and right child'''
+        self.data = data
+        self.left = None
+        self.right = None
